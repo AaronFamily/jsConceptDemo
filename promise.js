c9173972e344902911797cc7f190612f5c9adc75
aaron <aarongithub@yeah.net>
2018-04-08 12:36:37 +0800

    Promise

--- promise.js
+++ promise.js
@@ -1,6 +1,10 @@
 function Promise(executor){
 
+    if(typeof executor !== 'function') throw TypeError(executor + ' is not a function');
+
+
     var self = this;
+
 
     self.status = 'pending';
     self.result = undefined;
@@ -8,81 +12,134 @@ function Promise(executor){
     self.onResolvedCallbacks = [];
     self.onRejectedCallbacks = [];
 
-    if(typeof executor !== 'function') throw TypeError(executor + ' is not a function');
-
-    if (!(this instanceof Promise)) return new Promise(executor);
 
     var resolve = function(result){
         setTimeout(function(){
 
             if(self.status !== 'pending') return;
             
-            self.status = 'resolve';
+            self.status = 'fulfilled';
             self.result = result;
 
             var i = 0,
                 len = self.onResolvedCallbacks.length;
+                
 
-            for(; i < len; i++) {
-                self.onResolvedCallbacks[i]();
-            }
+            for(; i < len; i++) self.onResolvedCallbacks[i]();
         })
-    }
+    },
 
-    var reject = function(error){
+    reject = function(error){
         setTimeout(function(){
 
             if(self.status !== 'pending') return;
 
-            self.status = 'reject';
+            self.status = 'rejected';
             self.error = error;
 
             var i = 0,
                 len = self.onRejectedCallbacks.length;
+                
 
-            for(; i < len; i++) {
-                self.onRejectedCallbacks[i]();
-            }
+            for(; i < len; i++) self.onRejectedCallbacks[i]();
         })
     }
+
 
     try {
     
         executor(resolve,reject);
     } catch (err) {
-        reject(err)
+    
+        reject(err);
     }
 }
 
 Promise.prototype.then = function(onResolved,onRjected){
 
-    var self = this;
-    var promise2 = null;
+    var self = this,
+        
+        promise2 = null,
+        
+        reslovePromise = function (promise2, transfer, resolve, reject) {
+            
+            if(promise2 === transfer) throw TypeError(executor + ' is not a function');
+
+            var once = false;
+
+            if(transfer !== null && (Object.prototype.toString(transfer) === '[Object Object]' || typeof transfer === 'function')) {
+
+                try {
+
+                    if(!object.hasOwnProperty(pro) && pro in object) {
+                    
+                        transfer.then(function(result){
+
+                            
+                            if(once) return;
+    
+                            once = true;
+    
+                            reslovePromise(promise2, result, resolve, reject);
+                            var transfer = onRjected(self.error);
+
+                
+                            if(transfer instanceof Promise) transfer.then(resolve,reject);
+                        },function(err){
+                            if(once) return;
+    
+                            once = true;
+    
+                            reject(err);
+                        })
+                        
+                    }else{
+                        
+                        resolve(transfer);
+                    }
+                } catch (err) {
+                    var transfer = onRjected(self.error);
+
+                
+                    if(transfer instanceof Promise) transfer.then(resolve,reject);
+
+                    if (once) return
+
+                    once = true;
+                    
+                    reject(err);
+                }
+            }else{
+                
+                resolve(transfer);
+            }
+        };
 
     
 
     onResolved = typeof onResolved === 'function' ? onResolved : function (result) {
+
         return result;
     }
 
     onRjected = typeof onRjected === 'function' ? onRjected : function (err) {
+
         throw err;
     }
 
 
     if(self.status === 'pending') {
+
         promise2 = new Promise(function(resolve, reject) {
+
             self.onResolvedCallbacks.push(function () {
 
                 try{
+
                     var transfer = onResolved(self.result);
+
     
-    
-                    if (transfer instanceof Promise) {
-                        transfer.then(resolve,reject);
-                    }
-                    
-                    resolve(transfer);
+                    resolvePromise(promise2, transfer, resolve, reject)
     
                 }catch(err){
                     
@@ -93,13 +150,14 @@ Promise.prototype.then = function(onResolved,onRjected){
             self.onRejectedCallbacks.push(function () {
 
                 try {
-                    var transfer = onRjected(self.error);
+                    
+                    var transfer = onResolved(self.result);
+
     
-                    if(transfer instanceof Promise) {
-                        transfer.then(resolve,reject);
-                    }
+                    resolvePromise(promise2, transfer, resolve, reject)
                     
                 } catch (err) {
+
                     reject(err);
                 }
 
@@ -107,17 +165,16 @@ Promise.prototype.then = function(onResolved,onRjected){
         })
     }
 
-    if(self.status === 'resolve') {
+    if (self.status === 'fulfilled') {
+
         promise2 = new Promise(function(resolve, reject) {
+
             try{
+                 
                 var transfer = onResolved(self.result);
 
-
-                if (transfer instanceof Promise) {
-                    transfer.then(resolve,reject);
-                }
-                
-                resolve(transfer);
+    
+                resolvePromise(promise2, transfer, resolve, reject)
 
             }catch(err){
                 
@@ -126,21 +183,88 @@ Promise.prototype.then = function(onResolved,onRjected){
         })
     }
 
-    if(self.status === 'reject') {
-        promise2 = new Promise(function(resolve, reject) {
-            try {
-                var transfer = onRjected(self.error);
+    if (self.status === 'rejected') {
 
-                
-                if(transfer instanceof Promise) {
-                    transfer.then(resolve,reject)
-                }
+        promise2 = new Promise(function(resolve, reject) {
+
+            try {
+                 
+                var transfer = onResolved(self.result);
+
+    
+                resolvePromise(promise2, transfer, resolve, reject)
                 
             } catch (err) {
+
                 reject(err);
             }
         })
     }
 
+
     return promise2;
+}
+
+Promise.prototype.catch = function (errorCallback) {
+
+    return this.then(null, errorCallback);
+}
+
+Promise.resolve = function (result) {
+
+    return new Promise(function (resolve, reject) {
+
+        resolve(result);
+    })
+}
+
+Promise.reject = function (err) {
+
+    return new Promise(function (resolve, reject) {
+        
+        reject(err);
+    })
+}
+
+Promise.all = function (promoses) {
+
+    return new Promise(function (resolve, reject) {
+
+        var aar = [],
+            i = 0,
+            len = promoses.length;
+
+
+        for(; i < len; i++) {
+            
+            (function (index, promoses) {
+
+                Promise.resolve(promoses[index]).then(function (result) {
+
+                    aar[index] = result;
+
+                    if(++index === len) resolve(arr);
+
+                },function (err) {
+            
+                    reject(err);
+                })
+
+            }(i, promoses))
+
+        }
+    })
+}
+
+Promise.race = function (promoses) {
+    
+    return new Promise(function () {
+
+        var i = 0,
+            len = promoses.length;
+
+
+        for(; i < len; i++) promoses[i].then(resolve, reject);
+
+    })
 }
